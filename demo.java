50cdcd529087859f25804f36ed57fd7c8ed7b10e
Mohammed Samiullah <samiullah.mhasan@gmail.com>
2023-10-29 11:34:34 +0530

    Added new file

--- demo.java
+++ demo.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class demo{
     public static void main(String[] args){
         System.out.println("This is from Java program");
-        System.out.print(new Date());
-        System.out.print("Added new line");
+        System.out.println(new Date());
+        System.out.println("Added new line");
+        student s = new student();
+        s.sayHello();
     }
 }
---
+++ student.java
@@ -0,0 +1,9 @@
+public class student {
+    public student(){
+        System.out.println("Hello frmo student");
+    }
+
+    public void sayHello(){
+        System.out.println("Hi from Hello");
+    }
+}
