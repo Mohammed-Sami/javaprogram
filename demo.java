9394e4dc27e1706a3d0f45bada5cfdb8bc763a58
Mohammed Samiullah <samiullah.mhasan@gmail.com>
2023-10-29 11:29:34 +0530

    added new line

--- demo.java
+++ demo.java
@@ -4,5 +4,6 @@ public class demo{
     public static void main(String[] args){
         System.out.println("This is from Java program");
         System.out.print(new Date());
+        System.out.print("Added new line");
     }
 }
