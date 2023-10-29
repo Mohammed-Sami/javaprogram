6874087ac08130f177862db4a19778c35a93a7a5
Mohammed Samiullah <samiullah.mhasan@gmail.com>
2023-10-29 11:17:30 +0530

    First Commit

---
+++ demo.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class demo{
+    public static void main(String[] args){
+        System.out.println("This is from Java program");
+        System.out.print(new Date());
+    }
+}
